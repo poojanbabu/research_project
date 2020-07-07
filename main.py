eee7eb672254301e0c6f7bbf8f8d261edde12d16
Pooja N Babu <poojanbabu24@gmail.com>
2020-07-07 12:12:07 +0100

    Refactor code Functions for different kinds of runs.

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python
 import numpy as np
 import MyFunction as MyF
+from pathlib import Path
 
 nDimension = 1000
 nPattern = 1000
-nRun = 1  # one file contains events with one hyperplane solution
+nRun = 10  # one file contains events with one hyperplane solution
 learning_rate = 1.0
 size_buffer = 1000
 energy_scale_lLTP = 1.0
 energy_scale_maintenance = energy_scale_lLTP * np.array([0.001])
 synapse_threshold = 2
-arr_decay_eLTP = 0.001
+decay_eLTP = 0.001
 
 synapse_threshold_max = 40
-decay_rates = np.arange(0, 0.005, 0.001)
-learning_rates = np.array([0.1, 0.5, 1.0, 1.5, 2.0])
 
 #####################################
 #        Pattern generation         #
@@ -25,60 +24,182 @@ for iRun in range(nRun):
     pattern[iRun], pattern_answer[iRun] = MyF.MakePattern(nPattern, nDimension, is_pattern_integer=True,
                                                           is_plus_minus_one=True)
 
+
 #####################################
 #          Perceptron part          #
 #####################################
-weight_initial = np.zeros(nDimension + 1)  # +1 is for bias
-Per = MyF.Perceptron(pattern[0], pattern_answer[0], weight_initial, \
-                     size_buffer, learning_rate, energy_scale_lLTP, \
-                     energy_detail=True)
-Per.energy_scale_maintenance = energy_scale_maintenance
-# arr_energy = np.nan*np.ones(nRun) # total energy
-# arr_energy_eLTP = np.nan*np.ones(nRun) # transient energy
-# arr_energy_lLTP = np.nan*np.ones(nRun) # permanent energy
-# arr_error = np.nan*np.ones(nRun) # number of errors
-# arr_epoch = np.nan*np.ones(nRun) # training time (epoch)
+def synapse_thr_with_decay_rates():
+    decay_rates = np.arange(0, 0.005, 0.001)
 
-arr_energy = np.nan * np.ones(synapse_threshold_max)  # total energy
-arr_energy_eLTP = np.nan * np.ones(synapse_threshold_max)  # transient energy
-arr_energy_lLTP = np.nan * np.ones(synapse_threshold_max)  # permanent energy
-arr_error = np.nan * np.ones(synapse_threshold_max)  # number of errors
-arr_epoch = np.nan * np.ones(synapse_threshold_max)  # training time (epoch)
+    weight_initial = np.zeros(nDimension + 1)  # +1 is for bias
+    Per = MyF.Perceptron(pattern[0], pattern_answer[0], weight_initial, \
+                         size_buffer, learning_rate, energy_scale_lLTP, \
+                         energy_detail=True)
+    Per.energy_scale_maintenance = energy_scale_maintenance
 
-for decay_rate in decay_rates:
-    Per.decay_eLTP = decay_rate
-    for iRun in range(synapse_threshold_max):
-        Per.pattern = pattern[0]
-        Per.pattern_answer = pattern_answer[0]
-        arr_energy[iRun], arr_energy_eLTP[iRun], arr_energy_lLTP[iRun], arr_error[iRun], arr_epoch[
-            iRun] = Per.AlgoSynapseAll(iRun)
-        print(iRun, " energy:", arr_energy[iRun], " energy_eLTP:", arr_energy_eLTP[iRun], " energy_lLTP:",
-              arr_energy_lLTP[iRun], " error:", arr_error[iRun], " epoch:", arr_epoch[iRun])
+    arr_energy = np.nan * np.ones(synapse_threshold_max)  # total energy
+    arr_energy_eLTP = np.nan * np.ones(synapse_threshold_max)  # transient energy
+    arr_energy_lLTP = np.nan * np.ones(synapse_threshold_max)  # permanent energy
+    arr_error = np.nan * np.ones(synapse_threshold_max)  # number of errors
+    arr_epoch = np.nan * np.ones(synapse_threshold_max)  # training time (epoch)
 
-    np.savetxt("Text/energy_" + str(decay_rate) + ".txt", arr_energy)
-    np.savetxt("Text/energy_eLTP_" + str(decay_rate) + ".txt", arr_energy_eLTP)
-    np.savetxt("Text/energy_lLTP_" + str(decay_rate) + ".txt", arr_energy_lLTP)
-    np.savetxt("Text/error_" + str(decay_rate) + ".txt", arr_error)
-    np.savetxt("Text/epoch_" + str(decay_rate) + ".txt", arr_epoch)
+    for decay_rate in decay_rates:
+        Per.decay_eLTP = decay_rate
+        for i_synapse_threshold in range(synapse_threshold_max):
+            energy = np.nan * np.ones(nRun)
+            energy_eLTP = np.nan * np.ones(nRun)
+            energy_lLTP = np.nan * np.ones(nRun)
+            error = np.nan * np.ones(nRun)
+            epoch = np.nan * np.ones(nRun)
+            for iRun in range(nRun):
+                print("Run: {}".format(iRun+1))
+                Per.pattern = pattern[iRun]
+                Per.pattern_answer = pattern_answer[iRun]
+                energy[iRun], energy_eLTP[iRun], energy_lLTP[iRun], error[iRun], epoch[iRun] = Per.AlgoSynapseAll(i_synapse_threshold)
 
-# Per.decay_eLTP = 0.001
-# for learning_rate in learning_rates:
-#     Per.learning_rate = learning_rate
-#     for iRun in range(synapse_threshold_max):
-#         Per.pattern = pattern[0]
-#         Per.pattern_answer = pattern_answer[0]
-#         arr_energy[iRun], arr_energy_eLTP[iRun], arr_energy_lLTP[iRun], arr_error[iRun], arr_epoch[
-#             iRun] = Per.AlgoSynapseAll(iRun)
-#         print(iRun, " energy:", arr_energy[iRun], " energy_eLTP:", arr_energy_eLTP[iRun], " energy_lLTP:",
-#               arr_energy_lLTP[iRun], " error:", arr_error[iRun], " epoch:", arr_epoch[iRun])
-#     np.savetxt("Text/energy_" + str(learning_rate) + ".txt", arr_energy)
-#     np.savetxt("Text/energy_eLTP_" + str(learning_rate) + ".txt", arr_energy_eLTP)
-#     np.savetxt("Text/energy_lLTP_" + str(learning_rate) + ".txt", arr_energy_lLTP)
-#     np.savetxt("Text/error_" + str(learning_rate) + ".txt", arr_error)
-#     np.savetxt("Text/epoch_" + str(learning_rate) + ".txt", arr_epoch)
+            arr_energy[i_synapse_threshold] = np.mean(energy)
+            arr_energy_eLTP[i_synapse_threshold] = np.mean(energy_eLTP)
+            arr_energy_lLTP[i_synapse_threshold] = np.mean(energy_lLTP)
+            arr_error[i_synapse_threshold] = np.mean(error)
+            arr_epoch[i_synapse_threshold] = np.mean(epoch)
+            print("Threshold:", i_synapse_threshold, " energy:", arr_energy[i_synapse_threshold], " energy_eLTP:", arr_energy_eLTP[i_synapse_threshold], " energy_lLTP:",
+                  arr_energy_lLTP[i_synapse_threshold], " error:", arr_error[i_synapse_threshold], " epoch:", arr_epoch[i_synapse_threshold])
 
-np.savetxt("Text/variables.txt", (
-nDimension, nPattern, nRun, learning_rate, energy_scale_lLTP, energy_scale_maintenance, synapse_threshold_max,
-arr_decay_eLTP))
-np.savetxt("Text/decay_rates.txt", decay_rates)
-np.savetxt("Text/learning_rates.txt", learning_rates)
+        np.savetxt("../Text/energy_" + str(decay_rate) + ".txt", arr_energy)
+        np.savetxt("../Text/energy_eLTP_" + str(decay_rate) + ".txt", arr_energy_eLTP)
+        np.savetxt("../Text/energy_lLTP_" + str(decay_rate) + ".txt", arr_energy_lLTP)
+        np.savetxt("../Text/error_" + str(decay_rate) + ".txt", arr_error)
+        np.savetxt("../Text/epoch_" + str(decay_rate) + ".txt", arr_epoch)
+
+    np.savetxt("../Text/decay_rates.txt", decay_rates)
+
+
+def synapse_thr_with_learning_rates():
+    learning_rates = np.array([0.1, 0.5, 1.0, 1.5, 2.0])
+
+    weight_initial = np.zeros(nDimension + 1)  # +1 is for bias
+    Per = MyF.Perceptron(pattern[0], pattern_answer[0], weight_initial, \
+                         size_buffer, learning_rate, energy_scale_lLTP, \
+                         energy_detail=True)
+    Per.energy_scale_maintenance = energy_scale_maintenance
+    Per.decay_eLTP = decay_eLTP
+
+    arr_energy = np.nan * np.ones(synapse_threshold_max)  # total energy
+    arr_energy_eLTP = np.nan * np.ones(synapse_threshold_max)  # transient energy
+    arr_energy_lLTP = np.nan * np.ones(synapse_threshold_max)  # permanent energy
+    arr_error = np.nan * np.ones(synapse_threshold_max)  # number of errors
+    arr_epoch = np.nan * np.ones(synapse_threshold_max)  # training time (epoch)
+
+    for i_learning_rate in learning_rates:
+        Per.learning_rate = i_learning_rate
+        for i_synapse_threshold in range(synapse_threshold_max):
+            energy = np.nan * np.ones(nRun)
+            energy_eLTP = np.nan * np.ones(nRun)
+            energy_lLTP = np.nan * np.ones(nRun)
+            error = np.nan * np.ones(nRun)
+            epoch = np.nan * np.ones(nRun)
+            for iRun in range(nRun):
+                print("Pattern: {}".format(iRun))
+                Per.pattern = pattern[iRun]
+                Per.pattern_answer = pattern_answer[iRun]
+                energy[iRun], energy_eLTP[iRun], energy_lLTP[iRun], error[iRun], epoch[iRun] = Per.AlgoSynapseAll(
+                    i_synapse_threshold)
+
+            arr_energy[i_synapse_threshold] = np.mean(energy)
+            arr_energy_eLTP[i_synapse_threshold] = np.mean(energy_eLTP)
+            arr_energy_lLTP[i_synapse_threshold] = np.mean(energy_lLTP)
+            arr_error[i_synapse_threshold] = np.mean(error)
+            arr_epoch[i_synapse_threshold] = np.mean(epoch)
+            print("Threshold:", i_synapse_threshold, " energy:", arr_energy[i_synapse_threshold], " energy_eLTP:",
+                  arr_energy_eLTP[i_synapse_threshold], " energy_lLTP:",
+                  arr_energy_lLTP[i_synapse_threshold], " error:", arr_error[i_synapse_threshold], " epoch:",
+                  arr_epoch[i_synapse_threshold])
+
+        np.savetxt("../Text/energy_" + str(i_learning_rate) + ".txt", arr_energy)
+        np.savetxt("../Text/energy_eLTP_" + str(i_learning_rate) + ".txt", arr_energy_eLTP)
+        np.savetxt("../Text/energy_lLTP_" + str(i_learning_rate) + ".txt", arr_energy_lLTP)
+        np.savetxt("../Text/error_" + str(i_learning_rate) + ".txt", arr_error)
+        np.savetxt("../Text/epoch_" + str(i_learning_rate) + ".txt", arr_epoch)
+
+    np.savetxt("../Text/learning_rates.txt", learning_rates)
+
+
+def transient_decay_rates():
+    decay_rates = np.arange(0, 0.005, 0.001)
+    maintenance_costs = [0.001, 0.01]
+
+    weight_initial = np.zeros(nDimension + 1)  # +1 is for bias
+    Per = MyF.Perceptron(pattern[0], pattern_answer[0], weight_initial, \
+                         size_buffer, learning_rate, energy_scale_lLTP, \
+                         energy_detail=True)
+
+
+    arr_energy = np.nan * np.ones(len(decay_rates))  # total energy
+    arr_energy_eLTP = np.nan * np.ones(len(decay_rates))  # transient energy
+    arr_energy_lLTP = np.nan * np.ones(len(decay_rates))  # permanent energy
+    arr_error = np.nan * np.ones(len(decay_rates))  # number of errors
+    arr_epoch = np.nan * np.ones(len(decay_rates))  # training time (epoch)
+    arr_opt_threshold = np.nan * np.ones(len(decay_rates))  # optimal threshold
+
+    Path("../Text/Fig3").mkdir(parents=True, exist_ok=True)
+    for i_maintenance_cost in maintenance_costs:
+        print("Maintenance cost: {}".format(i_maintenance_cost))
+        Per.energy_scale_maintenance = i_maintenance_cost
+        for idx in range(len(decay_rates)):
+            Per.decay_eLTP = decay_rates[idx]
+            arr_energy_syn = []
+            arr_energy_eLTP_syn = []
+            arr_energy_lLTP_syn = []
+            arr_error_syn = []
+            arr_epoch_syn = []
+            print('Decay rate: {}'.format(decay_rates[idx]))
+            for i_synapse_threshold in range(synapse_threshold_max):
+                energy = np.nan * np.ones(nRun)
+                energy_eLTP = np.nan * np.ones(nRun)
+                energy_lLTP = np.nan * np.ones(nRun)
+                error = np.nan * np.ones(nRun)
+                epoch = np.nan * np.ones(nRun)
+                for iRun in range(nRun):
+                    Per.pattern = pattern[iRun]
+                    Per.pattern_answer = pattern_answer[iRun]
+                    energy[iRun], energy_eLTP[iRun], energy_lLTP[iRun], error[iRun], epoch[iRun] = Per.AlgoSynapseAll(i_synapse_threshold)
+
+                if np.mean(error) > 0.0:
+                    break
+
+                arr_energy_syn.append(np.mean(energy))
+                arr_energy_eLTP_syn.append(np.mean(energy_eLTP))
+                arr_energy_lLTP_syn.append(np.mean(energy_lLTP))
+                arr_error_syn.append(np.mean(error))
+                arr_epoch_syn.append(np.mean(epoch))
+                print("Threshold:", i_synapse_threshold, " energy:", arr_energy_syn[i_synapse_threshold], " energy_eLTP:", arr_energy_eLTP_syn[i_synapse_threshold], " energy_lLTP:",
+                      arr_energy_lLTP_syn[i_synapse_threshold], " error:", arr_error_syn[i_synapse_threshold], " epoch:", arr_epoch_syn[i_synapse_threshold])
+
+            min_idx = arr_energy_syn.index(min(arr_energy_syn))
+            arr_energy[idx] = arr_energy_syn[min_idx]
+            arr_energy_eLTP[idx] = arr_energy_eLTP_syn[min_idx]
+            arr_energy_lLTP[idx] = arr_energy_lLTP_syn[min_idx]
+            arr_error[idx] = arr_error_syn[min_idx]
+            arr_epoch[idx] = arr_epoch_syn[min_idx]
+            arr_opt_threshold[idx] = min_idx
+
+            print("Optimal Threshold:", min_idx, " energy:", arr_energy[idx], " energy_eLTP:",
+                  arr_energy_eLTP[idx], " energy_lLTP:",
+                  arr_energy_lLTP[idx], " error:", arr_error[idx], " epoch:",
+                  arr_epoch[idx])
+
+        np.savetxt("../Text/Fig3/energy_" + str(i_maintenance_cost) + ".txt", arr_energy)
+        np.savetxt("../Text/Fig3/energy_eLTP_" + str(i_maintenance_cost) + ".txt", arr_energy_eLTP)
+        np.savetxt("../Text/Fig3/energy_lLTP_" + str(i_maintenance_cost) + ".txt", arr_energy_lLTP)
+        np.savetxt("../Text/Fig3/error_" + str(i_maintenance_cost) + ".txt", arr_error)
+        np.savetxt("../Text/Fig3/epoch_" + str(i_maintenance_cost) + ".txt", arr_epoch)
+        np.savetxt("../Text/Fig3/opt_threshold_" + str(i_maintenance_cost) + ".txt", arr_opt_threshold)
+
+    np.savetxt("../Text/decay_rates.txt", decay_rates)
+    np.savetxt("../Text/maintenance_costs.txt", maintenance_costs)
+
+
+transient_decay_rates()
+np.savetxt("../Text/variables.txt", (
+    nDimension, nPattern, nRun, learning_rate, energy_scale_lLTP, energy_scale_maintenance, synapse_threshold_max,
+    decay_eLTP))
--- makeplot.py
+++ makeplot.py
@@ -2,11 +2,13 @@
 import math
 import matplotlib.pyplot as plt
 import numpy as np
+from pathlib import Path
 
 nDimension, nPattern, nRun, learning_rate, energy_scale_lLTP, energy_scale_maintenance, synapse_threshold, arr_decay_eLTP = np.loadtxt(
-    "Text/variables.txt")
-decay_rates = np.loadtxt("Text/decay_rates.txt")
-learning_rates = np.loadtxt("Text/learning_rates.txt")
+    "../Text/variables.txt")
+decay_rates = np.loadtxt("../Text/decay_rates.txt")
+learning_rates = np.loadtxt("../Text/learning_rates.txt")
+maintenance_costs = np.loadtxt("../Text/maintenance_costs.txt")
 nRun = int(synapse_threshold)
 
 arr_color = ["black", "red", "lawngreen", "cyan", "purple"]
@@ -14,90 +16,131 @@ marker_types = ['.', ',', 'o', 'v', 'd']
 
 xvalue = np.linspace(1, nRun, nRun)
 
-###### Plot energy #######
-marker_legends = []
-for i in range(len(decay_rates)):
-    arr_energy = np.loadtxt("Text/energy_" + str(decay_rates[i]) + ".txt")
-    arr_energy_eLTP = np.loadtxt("Text/energy_eLTP_" + str(decay_rates[i]) + ".txt")
-    arr_energy_lLTP = np.loadtxt("Text/energy_lLTP_" + str(decay_rates[i]) + ".txt")
-    leg, = plt.plot(xvalue, arr_energy, color=arr_color[i], linewidth=1, label="Decay rate = " + str(decay_rates[i]))
-    # plt.plot(xvalue, arr_energy_eLTP, color=arr_color[1], linewidth=1, marker=marker_types[i])
-    # plt.plot(xvalue, arr_energy_lLTP, color=arr_color[2], linewidth=1, marker=marker_types[i])
-    marker_legends.append(leg)
-# plt.plot(xvalue, arr_epoch, color=arr_color[3], linewidth=3, label="Epoch")
-plt.xlabel("Threshold", fontsize=16)
-plt.ylabel("Energy", fontsize=16)
-plt.yscale("log")
-plt.xlim(0, nRun + 0.5)
-plt.legend(handles=marker_legends, fontsize=10, loc=3)
-plt.tight_layout()
-plt.savefig("Plot/energy_d.png")
-plt.close()
+def plot_with_syn_threshold():
+    ###### Plot energy #######
+    marker_legends = []
+    for i in range(len(decay_rates)):
+        arr_energy = np.loadtxt("../Text/energy_" + str(decay_rates[i]) + ".txt")
+        arr_energy_eLTP = np.loadtxt("../Text/energy_eLTP_" + str(decay_rates[i]) + ".txt")
+        arr_energy_lLTP = np.loadtxt("../Text/energy_lLTP_" + str(decay_rates[i]) + ".txt")
+        leg, = plt.plot(xvalue, arr_energy, color=arr_color[i], linewidth=1, label="Decay rate = " + str(decay_rates[i]))
+        # plt.plot(xvalue, arr_energy_eLTP, color=arr_color[1], linewidth=1, marker=marker_types[i])
+        # plt.plot(xvalue, arr_energy_lLTP, color=arr_color[2], linewidth=1, marker=marker_types[i])
+        marker_legends.append(leg)
+    # plt.plot(xvalue, arr_epoch, color=arr_color[3], linewidth=3, label="Epoch")
+    plt.xlabel("Threshold", fontsize=16)
+    plt.ylabel("Energy", fontsize=16)
+    plt.yscale("log")
+    plt.xlim(0, nRun + 0.5)
+    plt.legend(handles=marker_legends, fontsize=10, loc=3)
+    plt.tight_layout()
+    plt.savefig("../Plot/energy_d.png")
+    plt.close()
 
-###### Plot epochs (Learning time) #######
-for i in range(len(decay_rates)):
-    arr_epoch = np.loadtxt("Text/epoch_" + str(decay_rates[i]) + ".txt")
-    plt.plot(xvalue, arr_epoch, color=arr_color[i], linewidth=1, label="Decay rate = " + str(decay_rates[i]))
-plt.xlabel("Threshold", fontsize=16)
-plt.ylabel("Epochs", fontsize=16)
-plt.xlim(0, nRun + 0.5)
-plt.legend(fontsize=10, loc=3)
-plt.tight_layout()
-plt.savefig("Plot/epochs_d.png")
-plt.close()
+    ###### Plot epochs (Learning time) #######
+    for i in range(len(decay_rates)):
+        arr_epoch = np.loadtxt("../Text/epoch_" + str(decay_rates[i]) + ".txt")
+        plt.plot(xvalue, arr_epoch, color=arr_color[i], linewidth=1, label="Decay rate = " + str(decay_rates[i]))
+    plt.xlabel("Threshold", fontsize=16)
+    plt.ylabel("Epochs", fontsize=16)
+    plt.xlim(0, nRun + 0.5)
+    plt.legend(fontsize=10, loc=3)
+    plt.tight_layout()
+    plt.savefig("../Plot/epochs_d.png")
+    plt.close()
 
-###### Plot error #######
-for i in range(len(decay_rates)):
-    arr_error = np.loadtxt("Text/error_" + str(decay_rates[i]) + ".txt")
-    plt.plot(xvalue, arr_error, color=arr_color[i], linewidth=1, label="Decay rate = " + str(decay_rates[i]))
-plt.xlabel("Threshold", fontsize=16)
-plt.ylabel("Error", fontsize=16)
-plt.xlim(0, nRun + 0.5)
-plt.legend(fontsize=10, loc=2)
-plt.tight_layout()
-plt.savefig("Plot/error_d.png")
-plt.close()
+    ###### Plot error #######
+    for i in range(len(decay_rates)):
+        arr_error = np.loadtxt("../Text/error_" + str(decay_rates[i]) + ".txt")
+        plt.plot(xvalue, arr_error, color=arr_color[i], linewidth=1, label="Decay rate = " + str(decay_rates[i]))
+    plt.xlabel("Threshold", fontsize=16)
+    plt.ylabel("Error", fontsize=16)
+    plt.xlim(0, nRun + 0.5)
+    plt.legend(fontsize=10, loc=2)
+    plt.tight_layout()
+    plt.savefig("../Plot/error_d.png")
+    plt.close()
 
-###### Plot energy #######
-marker_legends = []
-for i in range(len(learning_rates)):
-    arr_energy = np.loadtxt("Text/energy_" + str(learning_rates[i]) + ".txt")
-    arr_energy_eLTP = np.loadtxt("Text/energy_eLTP_" + str(learning_rates[i]) + ".txt")
-    arr_energy_lLTP = np.loadtxt("Text/energy_lLTP_" + str(learning_rates[i]) + ".txt")
-    leg, = plt.plot(xvalue, arr_energy, color=arr_color[i], linewidth=1, label="Learning rate = " + str(learning_rates[i]))
-    # plt.plot(xvalue, arr_energy_eLTP, color=arr_color[1], linewidth=1, marker=marker_types[i])
-    # plt.plot(xvalue, arr_energy_lLTP, color=arr_color[2], linewidth=1, marker=marker_types[i])
-    marker_legends.append(leg)
-# plt.plot(xvalue, arr_epoch, color=arr_color[3], linewidth=3, label="Epoch")
-plt.xlabel("Threshold", fontsize=16)
-plt.ylabel("Energy", fontsize=16)
-plt.yscale("log")
-plt.xlim(0, nRun + 0.5)
-plt.legend(handles=marker_legends, fontsize=10, loc=3)
-plt.tight_layout()
-plt.savefig("Plot/energy_l.png")
-plt.close()
+    ###### Plot energy #######
+    marker_legends = []
+    for i in range(len(learning_rates)):
+        arr_energy = np.loadtxt("../Text/energy_" + str(learning_rates[i]) + ".txt")
+        arr_energy_eLTP = np.loadtxt("../Text/energy_eLTP_" + str(learning_rates[i]) + ".txt")
+        arr_energy_lLTP = np.loadtxt("../Text/energy_lLTP_" + str(learning_rates[i]) + ".txt")
+        leg, = plt.plot(xvalue, arr_energy, color=arr_color[i], linewidth=1, label="Learning rate = " + str(learning_rates[i]))
+        # plt.plot(xvalue, arr_energy_eLTP, color=arr_color[1], linewidth=1, marker=marker_types[i])
+        # plt.plot(xvalue, arr_energy_lLTP, color=arr_color[2], linewidth=1, marker=marker_types[i])
+        marker_legends.append(leg)
+    # plt.plot(xvalue, arr_epoch, color=arr_color[3], linewidth=3, label="Epoch")
+    plt.xlabel("Threshold", fontsize=16)
+    plt.ylabel("Energy", fontsize=16)
+    plt.yscale("log")
+    plt.xlim(0, nRun + 0.5)
+    plt.legend(handles=marker_legends, fontsize=10, loc=3)
+    plt.tight_layout()
+    plt.savefig("../Plot/energy_l.png")
+    plt.close()
 
-###### Plot epochs (Learning time) #######
-for i in range(len(learning_rates)):
-    arr_epoch = np.loadtxt("Text/epoch_" + str(learning_rates[i]) + ".txt")
-    plt.plot(xvalue, arr_epoch, color=arr_color[i], linewidth=1, label="Learning rate = " + str(learning_rates[i]))
-plt.xlabel("Threshold", fontsize=16)
-plt.ylabel("Epochs", fontsize=16)
-plt.xlim(0, nRun + 0.5)
-plt.legend(fontsize=10, loc=2)
-plt.tight_layout()
-plt.savefig("Plot/epochs_l.png")
-plt.close()
+    ###### Plot epochs (Learning time) #######
+    for i in range(len(learning_rates)):
+        arr_epoch = np.loadtxt("../Text/epoch_" + str(learning_rates[i]) + ".txt")
+        plt.plot(xvalue, arr_epoch, color=arr_color[i], linewidth=1, label="Learning rate = " + str(learning_rates[i]))
+    plt.xlabel("Threshold", fontsize=16)
+    plt.ylabel("Epochs", fontsize=16)
+    plt.xlim(0, nRun + 0.5)
+    plt.legend(fontsize=10, loc=2)
+    plt.tight_layout()
+    plt.savefig("../Plot/epochs_l.png")
+    plt.close()
 
-###### Plot error #######
-for i in range(len(learning_rates)):
-    arr_error = np.loadtxt("Text/error_" + str(learning_rates[i]) + ".txt")
-    plt.plot(xvalue, arr_error, color=arr_color[i], linewidth=1, label="Learning rate = " + str(learning_rates[i]))
-plt.xlabel("Threshold", fontsize=16)
-plt.ylabel("Error", fontsize=16)
-plt.xlim(0, nRun + 0.5)
-plt.legend(fontsize=10, loc=2)
-plt.tight_layout()
-plt.savefig("Plot/error_l.png")
-plt.close()
+    ###### Plot error #######
+    for i in range(len(learning_rates)):
+        arr_error = np.loadtxt("../Text/error_" + str(learning_rates[i]) + ".txt")
+        plt.plot(xvalue, arr_error, color=arr_color[i], linewidth=1, label="Learning rate = " + str(learning_rates[i]))
+    plt.xlabel("Threshold", fontsize=16)
+    plt.ylabel("Error", fontsize=16)
+    plt.xlim(0, nRun + 0.5)
+    plt.legend(fontsize=10, loc=2)
+    plt.tight_layout()
+    plt.savefig("../Plot/error_l.png")
+    plt.close()
+
+
+def plot_fig3():
+    Path("../Plot/Fig3").mkdir(parents=True, exist_ok=True)
+
+    # Plot energy
+    for i in range(len(maintenance_costs)):
+        arr_energy = np.loadtxt("../Text/Fig3/energy_" + str(maintenance_costs[i]) + ".txt")
+        plt.plot(decay_rates, arr_energy, color=arr_color[i], linewidth=1, label="Maintenance cost = " + str(maintenance_costs[i]))
+    plt.xlabel("Decay Rate", fontsize=16)
+    plt.ylabel("Energy", fontsize=16)
+    plt.legend(fontsize=10, loc=4)
+    plt.tight_layout()
+    plt.savefig("../Plot/Fig3/energy.png")
+    plt.close()
+
+    # Plot optimal threshold
+    for i in range(len(maintenance_costs)):
+        arr_opt_threshold = np.loadtxt("../Text/Fig3/opt_threshold_" + str(maintenance_costs[i]) + ".txt")
+        plt.plot(decay_rates, arr_opt_threshold, color=arr_color[i], linewidth=1, label="Maintenance cost = " + str(maintenance_costs[i]))
+    plt.xlabel("Decay Rate", fontsize=16)
+    plt.ylabel("Opt threshold", fontsize=16)
+    plt.legend(fontsize=10, loc=1)
+    plt.tight_layout()
+    plt.savefig("../Plot/Fig3/opt_threshold.png")
+    plt.close()
+
+    # Plot learning time
+    for i in range(len(maintenance_costs)):
+        arr_epoch = np.loadtxt("../Text/Fig3/epoch_" + str(maintenance_costs[i]) + ".txt")
+        plt.plot(decay_rates, arr_epoch, color=arr_color[i], linewidth=1, label="Maintenance cost = " + str(maintenance_costs[i]))
+    plt.xlabel("Decay Rate", fontsize=16)
+    plt.ylabel("Learning time", fontsize=16)
+    plt.legend(fontsize=10, loc=1)
+    plt.tight_layout()
+    plt.savefig("../Plot/Fig3/epoch.png")
+    plt.close()
+
+
+plot_fig3()
